503db879e38301c67f2a69f6072b537f59ebb6db
Gabayoyo <aarongaba05@gmail.com>
2026-06-17 20:06:23 +0100

    refactor: redid init file for proper exposing of API

--- src/__init__.py
+++ src/__init__.py
@@ -1,25 +1,5 @@
 # src/__init__.py
 # exposes the main functions of the package for easy import
-
-# Main functions for analysis
-from analysis.pose_estimator import PoseEstimator # class for estimating pose data from videos
-from analysis.compute_metrics import compute_metrics # main metric pipeline function
-from analysis.compute_rep import compute_rep_metrics # function for computing rep-level metrics from list of angles
-
-# Key DTOs
-from dto.frame_data import FrameData, Landmark
-from dto.results import RepAnalysisResult, RirAnalysisResult
-
-# Main class for pipeline
 from bigger_brother import BiggerBrother
 
-__all__ = [
-    "PoseEstimator",
-    "compute_metrics",
-    "compute_rep_metrics",
-    "FrameData",
-    "Landmark",
-    "RepAnalysisResult",
-    "RirAnalysisResult",
-    "BiggerBrother"
-]
+__all__ = ["BiggerBrother"]
